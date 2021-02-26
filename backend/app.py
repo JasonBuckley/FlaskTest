1c5fe4fad3b242519f77363c426de8bd8852c5c2
Jason Buckley <jbuckley@csumb.edu>
2021-02-26 05:01:00 -0800

    fixed backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from pml import os
  
 app = Flask(__name__, static_url_path='', static_folder='./build', template_folder='./build')
  
@@ -8,4 +9,5 @@ def hello():
     return render_template("index.html")
  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000));
+    app.run(debug=True, port=port)
